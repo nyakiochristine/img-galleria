3b8e43c62fb3b6419fde2aad9e3ba9306b293136
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-29 20:28:11 +0300

    ft:created a function for getting images

--- galleria/views.py
+++ galleria/views.py
@@ -3,6 +3,13 @@ from django.http import HttpResponse,Http404
 from .models import Location,Category,Image
 
 # Create your views here.
+def pictures(request):
+    images = Image.get_all_images()
+    location = Location.objects.all()
+    
+    return render(request,'images.html',{'images':images,'locations':location})
+
+
 #function for searching results
 def search_results(request):
     categories = Category.objects.all()
