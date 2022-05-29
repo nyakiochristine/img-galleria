3134166e5dd2d138ca5ae2e70be4f776866933bf
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-29 21:55:31 +0300

    added unittest for all model classes

--- galleria/tests.py
+++ galleria/tests.py
@@ -2,6 +2,33 @@ from django.test import TestCase
 from .models import Image,Category,Location
 
 # Create your tests here.
+class LocationTestCase(TestCase):
+    def setUp(self):
+        self.location = Location(location_name='kamakis')
+    
+    def tearDown(self):
+        Location.objects.all().delete()
+        
+        
+    #test the instance
+    def test_instance(self):
+        self.assertTrue(self.location,Location)
+        
+    
+    
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        self.category = Category(category_name='Business')
+        
+        
+    def tearDown(self):
+        Category.objects.all().delete()
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category,Category))
+
+
+
 
 class ImageTestCase(TestCase):
     def setUp(self):
@@ -12,6 +39,20 @@ class ImageTestCase(TestCase):
         self.category.save()
         
         self.image_test = Image(image_name='image_test',image_description='Testing the image',category=self.category,location=self.location)
+        
+    def test_instance(self):
+        self.AssertTrue(isinstance.self.image_test,Image)
+        
+        
+    def tearDown(self):
+        Image.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+        
+        
+        
+        
+    
         
     
         
