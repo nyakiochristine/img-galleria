d203f0383b1deeab8fed2dc0504083d648f85897
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-29 20:24:28 +0300

    ft:created a function for search results

--- galleria/views.py
+++ galleria/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse,Http404
 from .models import Location,Category,Image
 
 # Create your views here.
+#function for searching results
+def search_results(request):
+    categories = Category.objects.all()
+    location = Location.objects.all()
+    
