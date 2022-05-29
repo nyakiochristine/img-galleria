3fa7cdde6b1f170b2781ec3697f43cf456e757ca
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-29 13:38:40 +0300

    ft:created a location class

--- galleria/models.py
+++ galleria/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+#3 models location,category, image 
+
+#1location
+class Location(models.Model):
+    location_name = models.CharField(max_length=45)
+    def __str__(self):
+        self.location_name
+        
+    def save_location(self):
+        self.save
+        
+    def change_location(self):
+        self.change_location
+        
+    def delete_location(self):
+        self.delete_location
+        
+    
