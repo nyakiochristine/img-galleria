0d1325b437ca334fa7d1ec067783a382f9efd89e
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-29 17:30:25 +0300

    ft:import model classes

--- galleria/views.py
+++ galleria/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
+from .models import Location,Category,Image
 
 # Create your views here.
