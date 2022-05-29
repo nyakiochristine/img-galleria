89cca728a9fb568ce3e503f74f4e11329fe9bcf3
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-29 21:44:31 +0300

    ft:created a test for image model

--- galleria/tests.py
+++ galleria/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Image,Category,Location
 
 # Create your tests here.
+
+class ImageTestCase(TestCase):
+    def setUp(self):
+        self.location = Location(location_name= 'kamakis')
+        self.location.save()
+        
+        self.category = Category(category_name='Tourism')
+        self.category.save()
+        
+        self.image_test = Image(image_name='image_test',image_description='Testing the image',category=self.category,location=self.location)
+        
+    
+        
+    
